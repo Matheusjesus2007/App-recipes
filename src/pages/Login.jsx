c8cf74391d48c79df5cefa6cb6067cfe15d14a97
Matheus de Jesus <matheusjesus885@outlook.com>
2023-02-20 00:27:16 -0300

    Estilo da pagina de login finalizado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import styles from '../styles/Login.module.css';
+import openEye from '../images/fa-eye.png';
+import closedEye from '../images/fa-eye-slash.png';
 
 function Login({ history }) {
   const [user, setUser] = useState({ email: '', password: '' });
   const [errorMessageEmail, setErrorMessageEmai] = useState('');
   const [errorMessagePassword, setErrorMessagePassword] = useState('');
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [showPassWord, setShowPassWord] = useState(false);
 
   const regex = /\S+@\S+\.\S+/;
   const minPasswordLength = 6;
@@ -42,40 +47,71 @@ function Login({ history }) {
   }, [user.email, user.password]);
 
   return (
-    <section>
-      <label htmlFor="'input-email'">
-        <input
-          id="input-email"
-          type="email"
-          data-testid="email-input"
-          placeholder="Email:"
-          onChange={ handleChange }
-          name="email"
-          value={ user.email }
-        />
-      </label>
-      <p>{errorMessageEmail}</p>
-      <label htmlFor="input-senha">
-        <input
-          id="input-senha"
-          type="password"
-          data-testid="password-input"
-          placeholder="Senha:"
-          onChange={ handleChange }
-          name="password"
-          value={ user.password }
-        />
-      </label>
-      <p>{errorMessagePassword}</p>
-      <button
-        type="button"
-        disabled={ isButtonDisabled }
-        onClick={ submitButton }
-        data-testid="login-submit-btn"
-      >
-        Entrar
-      </button>
-    </section>
+    <main className={ styles.containerMain }>
+      <form className={ styles.containerForm }>
+        <h1>Welcome</h1>
+        <label
+          htmlFor="'input-email'"
+          className={ styles.labelEmail }
+        >
+          <input
+            id="input-email"
+            type="email"
+            data-testid="email-input"
+            placeholder="Email:"
+            onChange={ handleChange }
+            name="email"
+            value={ user.email }
+          />
+        </label>
+        <p className={ styles.errorMessage }>{errorMessageEmail}</p>
+        <label
+          htmlFor="input-senha"
+          className={ styles.labelPass }
+        >
+          <input
+            id="input-senha"
+            type={ showPassWord ? 'text' : 'password' }
+            data-testid="password-input"
+            placeholder="Senha:"
+            onChange={ handleChange }
+            name="password"
+            value={ user.password }
+          />
+          <button
+            className={ styles.btnEyes }
+            type="button"
+            onClick={ () => setShowPassWord(!showPassWord) }
+          >
+            <img
+              className={ styles.eyes }
+              src={ showPassWord ? openEye : closedEye }
+              alt={ showPassWord ? 'open Eye' : 'closed Eye' }
+
+            />
+          </button>
+        </label>
+        <p className={ styles.errorMessage }>{errorMessagePassword}</p>
+        <label htmlFor="input-remember" className={ styles.containerRemember }>
+          Remember me
+          <input
+            id="input-remember"
+            type="checkbox"
+            name="remember"
+          />
+        </label>
+        <Link to="*" className={ styles.forgotPassword }>Forgot password?</Link>
+        <button
+          type="button"
+          disabled={ isButtonDisabled }
+          onClick={ submitButton }
+          data-testid="login-submit-btn"
+          className={ styles.buttonSingIn }
+        >
+          Sign in
+        </button>
+      </form>
+    </main>
   );
 }
 
